00d8750181d9a07338e2d026900e4719cd66724c
HalidÜnal <halidunal@gmail.com>
2024-05-05 17:14:01 +0300

    typeDefs and resolvers

--- index.js
+++ index.js
@@ -1,31 +1,84 @@
 const {ApolloServer} = require('@apollo/server')
 const {startStandaloneServer} = require('@apollo/server/standalone')
-const {buildSchema} = require("graphql");
 
 const typeDefs = `#graphql
   type Book {
-    title: String
-    author: String
+    id:Int!
+    title: String!
+    author: String!
+    isActive: Boolean!
+    price: Float!
+    user: User!
+  }
+  type User {
+    id:Int!
+    name:String!
+  }
+  input CreateUserDataType{
+    name: String!
+  }
+  type Mutation{
+    createUser(data:CreateUserDataType):User!
   }
   type Query {
     books: [Book]
+    users: [User]
+    user(id:Int): Boolean!
   }
 `;
 
 const books = [
     {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
+        id: 1,
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+        isActive: true,
+        price: 2.5,
+        user: {
+            id: 1,
+            name: 'Kate',
+        }
     },
     {
-      title: 'City of Glass',
-      author: 'Paul Auster',
+        id: 2,
+        title: 'City of Glass',
+        author: 'Paul Auster',
+        isActive: false,
+        price: 5.2,
+        user: {
+            id: 2,
+            name: 'Paul',
+        }
     },
-  ];
+];
+
+const users = [
+    {
+        id: 1,
+        name: 'Kate',
+    },
+    {
+        id: 2,
+        name: 'Paul',
+    },
+];
 
 const resolvers={
+    Mutation:{
+        createUser(_,args){
+            users.push({
+                id: Date.now(),
+                name: args.data.name
+            })
+            return true
+        }
+    },
     Query:{
-        books:()=>books
+        user:(_,args) => {
+            return users.find(user => user.id === args.id)
+        },
+        books:()=>books,
+        users:()=>users
     }
 }
 
